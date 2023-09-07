bbafebf9fe28b073328de9a0c595eb206f97c255
Your Name <st117215@student.spbu.ru>
2023-09-07 20:46:56 +0300

    Homework 1_2

---
+++ 2023.09.09-homework-1/Project2/Project2/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	int n = 0;
+	int s = 0;
+	std::cin >> n;
+	s = (1 + n) / 2 * n;
+	std::cout << "summa chisel from 1 to " << n << " - " << s << std::endl;
+
+	return EXIT_SUCCESS;
+}
