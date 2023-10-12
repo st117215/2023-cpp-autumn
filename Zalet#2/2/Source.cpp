d1c5dfc1527ed2fa14e0e36b1cb8c5b9d35714a0
Your Name <st117215@student.spbu.ru>
2023-10-12 22:19:30 +0300

    Tasks from Zalet#2

--- Zalet#2/2/Source.cpp
+++ Zalet#2/2/Source.cpp
@@ -4,11 +4,21 @@ int main(int argc, char* argv[])
 {
 	int n = 0;
 	std::cin >> n;
-	for (int i = 0; i < n; i++) {
-		std::cout << i << ' ';
+	if (n > 0) {
+		for (int i = 0; i < n; i++) {
+			std::cout << i << ' ';
+		}
+		for (int i = n; i >= 0; i--) {
+			std::cout << i << ' ';
+		}
 	}
-	for (int i = n; i >= 0; i--) {
-		std::cout << i << ' ';
+	else{
+		for (int i = 0; i > n; i--) {
+			std::cout << i << ' ';
+		}
+		for (int i = n; i <= 0; i++) {
+			std::cout << i << ' ';
+		}
 	}
 	return 0;
 }
